9e7febae81443bde0c91271c80b105e26ab49311
aleblanc <aleblanc@interact.lu>
2016-05-09 18:04:43 +0200

    dev phase 1

---
+++ app/my-title.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+        selector: 'my-title',
+        template: `
+<div>
+    <h3>MON TITLE component</h3>
+</div>
+        `
+})
+export class MyTitleComponent {
+}
